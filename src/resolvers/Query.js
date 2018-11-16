8b377cb95511c27b44b42239ccce3fea143ae5c7
Max Goodman <max.goodman@flatironschool.com>
2018-11-16 17:36:27 -0500

    adding feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,7 +6,20 @@ function feed(parent, args, context, info){
 		{ description_contains: args.filter},
 		],
 	}: {}
-	return context.db.query.links({ where, skip: args.skip, first: args.first  }, info,)
+	const queriedLinks = await context.db.query.links({ where, skip: args.skip, first: args.first, orderBy: args.orderBy  }, `{id}`,)
+
+	const countSelectionSet = {
+		aggregate {
+			count
+		}
+	}
+
+	const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+	return {
+		count: linksConnection.aggregate.count, 
+		linkIds: queriedLinks.map(link => link.id),
+	}
 }
 
 module.exports ={
