7cea9e742fcdfdb06c9fab067d5cf421eab44291
Max Goodman <max.goodman@flatironschool.com>
2018-11-16 11:30:29 -0500

    adding User in datamodel.graphql

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
 	Query: {
@@ -23,6 +24,15 @@ const resolvers = {
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
 	resolvers,
+	context: req => ({
+		...req,
+		db: new Prisma({
+			typeDefs: 'src/generated/prisma.graphql',
+			endpoint: "https://us1.prisma.sh/maxagoodman/database/dev",
+			secret: 'mysecret123',
+			debug: true,
+		}),
+	}),
 })
 
 server.start(() => console.log('Server is running in http://localhost:4000'))
