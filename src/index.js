b02e327e6fc43b5904bf026dc00d0c46ba76b4d5
Max Goodman <max.goodman@flatironschool.com>
2018-11-16 11:09:56 -0500

    adding all files to git

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+const resolvers = {
+	Query: {
+		info: () => `This is the API of a Hackernews Clone`,
+		feed: () => (root, args, context, info) =>{
+			return context.db.query.links({}, info)
+		}
+	},
+
+	Mutation: {
+		post: (root, args, context, info) =>{
+			return context.db.mutation.createLink({
+				data: {
+					url: args.url,
+					description: args.description,
+				},
+				}, info)
+		},
+	},
+}
+
+const server = new GraphQLServer({
+	typeDefs: './src/schema.graphql',
+	resolvers,
+})
+
+server.start(() => console.log('Server is running in http://localhost:4000'))
